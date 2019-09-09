227e656fac878ff5ce82105c45e3b0b00eaf07f7
andrewdl <andrewdl@umich.edu>
2019-09-09 17:40:07 -0400

    print stuff

--- helloworld.py
+++ helloworld.py
@@ -1,11 +1,11 @@
 import random
 
-print "Andrew"
+print("Andrew")
 num1 = random.randint(0, 100)
 num2 = random.randint(0, 100)
-print num1
-print num2
+print(num1)
+print(num2)
 sum = num1 + num2
 avg = sum/2.0
-print "Sum = " + str(sum)
-print "Average = " + str(avg)
+print("Sum = " + str(sum))
+print("Average = " + str(avg))
